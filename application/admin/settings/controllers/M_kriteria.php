94c060fcd5c06b9bfa4ae87658560c17d08bc964
taufikmaulanaa <taufikm9977@icloud.id>
2025-11-04 16:58:50 +0700

    Add M_kriteria controller and view: implement CRUD operations, import/export functionality, and enhance form fields for detailed data entry.

---
+++ application/admin/settings/controllers/M_kriteria.php
@@ -0,0 +1,84 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class M_kriteria extends BE_Controller {
+
+	function __construct() {
+		parent::__construct();
+	}
+
+	function index() {
+		render();
+	}
+
+	function data() {
+		$data = data_serverside();
+		render($data,'json');
+	}
+
+	function get_data() {
+		$data = get_data('tbl_kriteria','id',post('id'))->row_array();
+		render($data,'json');
+	}
+
+	function save() {
+		$response = save_data('tbl_kriteria',post(),post(':validation'));
+		render($response,'json');
+	}
+
+	function delete() {
+		$response = destroy_data('tbl_kriteria','id',post('id'));
+		render($response,'json');
+	}
+
+	function template() {
+		ini_set('memory_limit', '-1');
+		$arr = ['detail' => 'detail','location' => 'location','reff_name' => 'reff_name','publisher' => 'publisher','effective_date' => 'effective_date','created_by' => 'created_by','created_at' => 'created_at'];
+		$config[] = [
+			'title' => 'template_import_m_kriteria',
+			'header' => $arr,
+		];
+		$this->load->library('simpleexcel',$config);
+		$this->simpleexcel->export();
+	}
+
+	function import() {
+		ini_set('memory_limit', '-1');
+		$file = post('fileimport');
+		$col = ['detail','location','reff_name','publisher','effective_date','created_by','created_at'];
+		$this->load->library('simpleexcel');
+		$this->simpleexcel->define_column($col);
+		$jml = $this->simpleexcel->read($file);
+		$c = 0;
+		foreach($jml as $i => $k) {
+			if($i==0) {
+				for($j = 2; $j <= $k; $j++) {
+					$data = $this->simpleexcel->parsing($i,$j);
+					$data['create_at'] = date('Y-m-d H:i:s');
+					$data['create_by'] = user('nama');
+					$save = insert_data('tbl_kriteria',$data);
+					if($save) $c++;
+				}
+			}
+		}
+		$response = [
+			'status' => 'success',
+			'message' => $c.' '.lang('data_berhasil_disimpan').'.'
+		];
+		@unlink($file);
+		render($response,'json');
+	}
+
+	function export() {
+		ini_set('memory_limit', '-1');
+		$arr = ['detail' => 'Detail','location' => 'Location','reff_name' => 'Reff Name','publisher' => 'Publisher','effective_date' => '-dEffective Date','created_by' => 'Created By','created_at' => '-dCreated At'];
+		$data = get_data('tbl_kriteria')->result_array();
+		$config = [
+			'title' => 'data_m_kriteria',
+			'data' => $data,
+			'header' => $arr,
+		];
+		$this->load->library('simpleexcel',$config);
+		$this->simpleexcel->export();
+	}
+
+}
---
+++ application/admin/settings/views/m_kriteria/index.php
@@ -0,0 +1,55 @@
+<div class="content-header">
+	<div class="main-container position-relative">
+		<div class="header-info">
+			<div class="content-title"><?php echo $title; ?></div>
+			<?php echo breadcrumb(); ?>
+		</div>
+		<div class="float-right">
+			<?php echo access_button('delete,active,inactive,export,import'); ?>
+		</div>
+		<div class="clearfix"></div>
+	</div>
+</div>
+<div class="content-body">
+	<?php
+	table_open('',true,base_url('settings/m_kriteria/data'),'tbl_kriteria');
+		thead();
+			tr();
+				th('checkbox','text-center','width="30" data-content="id"');
+				th(lang('detail'),'','data-content="detail"');
+				th(lang('location'),'','data-content="location"');
+				th(lang('reff_name'),'','data-content="reff_name"');
+				th(lang('publisher'),'','data-content="publisher"');
+				th(lang('effective_date'),'','data-content="effective_date" data-type="daterange"');
+				th(lang('created_by'),'','data-content="created_by"');
+				th(lang('created_at'),'','data-content="created_at" data-type="daterange"');
+				th('&nbsp;','','width="30" data-content="action_button"');
+	table_close();
+	?>
+</div>
+<?php 
+modal_open('modal-form');
+	modal_body();
+		form_open(base_url('settings/m_kriteria/save'),'post','form');
+			col_init(3,9);
+			input('hidden','id','id');
+			textarea(lang('detail'),'detail');
+			input('text',lang('location'),'location');
+			input('text',lang('reff_name'),'reff_name');
+			input('text',lang('publisher'),'publisher');
+			input('datetime',lang('effective_date'),'effective_date');
+			input('text',lang('created_by'),'created_by');
+			input('datetime',lang('created_at'),'created_at');
+			form_button(lang('simpan'),lang('batal'));
+		form_close();
+	modal_footer();
+modal_close();
+modal_open('modal-import',lang('impor'));
+	modal_body();
+		form_open(base_url('settings/m_kriteria/import'),'post','form-import');
+			col_init(3,9);
+			fileupload('File Excel','fileimport','required','data-accept="xls|xlsx"');
+			form_button(lang('impor'),lang('batal'));
+		form_close();
+modal_close();
+?>
